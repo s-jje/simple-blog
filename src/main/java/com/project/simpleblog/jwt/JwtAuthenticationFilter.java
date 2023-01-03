f4533aae5283f7c692fe67d00587d8c2b3911911
sjcho <sshsjhome@gmail.com>
2023-01-03 23:59:00 +0900

    Feat: JWT 필터 JWT 인증 필터 추가

---
+++ src/main/java/com/project/simpleblog/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.project.simpleblog.jwt;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if (token != null) {
+            if (!jwtTokenProvider.isValidToken(token)) {
+                return;
+            }
+            Claims info = jwtTokenProvider.getUserInfoFromToken(token);
+            setAuthentication(info.getSubject());
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtTokenProvider.createAuthentication(username);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+}
