7a664688bca141b419106a50597bd3c6fa3e8005
Lim DongGyu <ldg1160296@gmail.com>
2023-01-05 00:13:09 +0900

    test

--- src/main/java/com/project/simpleblog/controller/BoardApiController.java
+++ src/main/java/com/project/simpleblog/controller/BoardApiController.java
@@ -5,11 +5,15 @@ import com.project.simpleblog.dto.BoardResponseDto;
 import com.project.simpleblog.dto.StatusResponseDto;
 import com.project.simpleblog.security.UserDetailsImpl;
 import com.project.simpleblog.service.BoardService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -43,5 +47,11 @@ public class BoardApiController {
     public StatusResponseDto deleteBoard(@PathVariable("id") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return boardService.delete(id, userDetails.getUser());
     }
+
+    @PatchMapping("/boards/{id}/likes")
+    public ResponseEntity<String> updateBoardLike(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return new ResponseEntity<>(boardLikeService.updateBoardLike(id,userDetails), HttpStatus.OK);
+    }
+
 
 }
