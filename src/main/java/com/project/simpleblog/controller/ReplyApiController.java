a2a0ca4ccf9ccd8e3abaea2b80720792ea200b07
sjcho <sshsjhome@gmail.com>
2023-01-05 00:19:19 +0900

    Feat: 대댓글 기능 대댓글 기능 추가

---
+++ src/main/java/com/project/simpleblog/controller/ReplyApiController.java
@@ -0,0 +1,35 @@
+package com.project.simpleblog.controller;
+
+import com.project.simpleblog.dto.ReplyRequestDto;
+import com.project.simpleblog.dto.ReplyResponseDto;
+import com.project.simpleblog.dto.StatusResponseDto;
+import com.project.simpleblog.security.UserDetailsImpl;
+import com.project.simpleblog.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/boards")
+@RequiredArgsConstructor
+public class ReplyApiController {
+
+    private final ReplyService replyService;
+
+    @PostMapping("/{boardId}/comments/{commentId}/replies")
+    public ReplyResponseDto registerReply(@PathVariable Long boardId, @PathVariable Long commentId, @RequestBody ReplyRequestDto replyRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return replyService.register(commentId, new ReplyRequestDto(replyRequestDto.getContent()), userDetails.getUser());
+    }
+
+    @PatchMapping("/{boardId}/comments/{commentId}/replies/{replyId}")
+    public ReplyResponseDto updateReply(@PathVariable Long boardId, @PathVariable Long commentId, @PathVariable Long replyId, @RequestBody ReplyRequestDto replyRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return replyService.update(commentId, replyId, replyRequestDto, userDetails.getUser());
+    }
+
+    @DeleteMapping("/{boardId}/comments/{commentId}/replies/{replyId}")
+    public StatusResponseDto deleteReply(@PathVariable Long boardId, @PathVariable Long commentId, @PathVariable Long replyId, BindingResult bindingResult, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return replyService.delete(commentId, replyId, userDetails.getUser());
+    }
+
+}
